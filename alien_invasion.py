9b0f4fdcf84c6f6dfa33f84d1be96612507d2a69
KeijiPlata <lordmiackykeijip@gmail.com>
2023-01-30 02:40:27 +0800

    Adding movement to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,6 @@
 # Alien Invasion
+
+# importing the packages needed
 import sys
 import pygame
 
@@ -38,6 +40,11 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # checks for keypress
+            elif event.type == pygame.KEYDOWN:
+                if event.key ==  pygame.K_RIGHT:
+                    # move the ship to the right
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images, flip to the new screen"""
