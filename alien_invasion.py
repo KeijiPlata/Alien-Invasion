9a610ccbed9b07036cfacdfa8671aae91b09ae67
KeijiPlata <lordmiackykeijip@gmail.com>
2023-02-20 12:08:20 +0800

    Fullscreem for the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Full screen for the game
+        # get the size of the screen
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         self.settings.screen_height=self.screen.get_rect().height
         self.settings.screen_width=self.screen.get_rect().width
